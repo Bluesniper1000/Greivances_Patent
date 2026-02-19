e32a5a32bd4d2c6f72eaabe708449e207758eb3e
Bluesniper1000 <anubhabsaha1000@gmail.com>
2026-02-19 12:44:59 +0530

    updated requirements

--- Src/Embeddings/Semantic_embedder.py
+++ Src/Embeddings/Semantic_embedder.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import numpy as np
-from gensim.models import fasttext
-
-class FastTextEncoder:
-    def __init__(self, model_path: str):
-=======
 """
 Semantic embedding using FastText pretrained models.
 """
@@ -26,30 +19,10 @@ class FastTextEncoder:
         Args:
             model_path: Path to pretrained FastText model
         """
->>>>>>> upstream/main
         print("Loading FastText model (this may take a minute)")
         self.model = fasttext.load_facebook_model(model_path)
         self.vector_size = self.model.vector_size
 
-<<<<<<< HEAD
-    def encode_tokens(self, tokens: list[str]) -> np.ndarray:
-        vectors = [self.model.wv[token] for token in tokens]
-        return np.array(vectors)
-
-    def get_embedding_matrix(self, word_index: dict[str, int]) -> np.ndarray:
-        """Creates the weight matrix for your Keras Embedding layer."""
-        vocab_size = len(word_index) + 1
-        embedding_matrix = np.zeros((vocab_size, self.vector_size))
-        
-        for word, i in word_index.items():
-            if word in self.model.wv:
-                embedding_matrix[i] = self.model.wv[word]
-            else:
-                embedding_matrix[i] = self.model.wv.get_vector(word)
-                
-        return embedding_matrix
-    
-=======
     def encode_tokens(self, tokens: List[str]) -> np.ndarray:
         """
         Convert tokens to embedding vectors.
@@ -111,4 +84,8 @@ class FastTextEncoder:
                 embedding_matrix[i] = self.model.wv.get_vector(word)
                 
         return embedding_matrix
->>>>>>> upstream/main
+
+    class Semantic:
+        def __init__():
+            pass
+        
